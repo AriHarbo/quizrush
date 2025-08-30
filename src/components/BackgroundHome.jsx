a35c223c8bd0f866d4a1b31943c41f284f061efc
AriHarbo <arielharbo.219@gmail.com>
2025-08-30 17:06:55 -0300

    Implementacion responsive del home y animaciones a los botones

--- src/components/BackgroundHome.jsx
+++ src/components/BackgroundHome.jsx
@@ -2,11 +2,25 @@ import React from 'react'
 
 const BackgroundHome = () => {
   return (
-    <div className="absolute inset-0 -z-10 overflow-hidden">
-        <div className="absolute -top-55 -left-55 w-160 h-160 bg-gradient-to-br from-orange-300 via-orange-400 to-orange-300 rounded-full opacity-60"></div>
-        <div className="absolute top-50 -left-12 w-110 h-110 bg-gradient-to-br from-orange-300 via-orange-400 to-orange-300 rounded-full opacity-40"></div>
-        <div className="absolute  top-1/6 -right-20 w-80 h-80 bg-gradient-to-br from-yellow-300 via-orange-200 to-yellow-400 rounded-full opacity-50"></div>
-        <div className="absolute -bottom-30 -right-10 w-110 h-110 bg-gradient-to-br from-yellow-300 via-cyan-900 to-yellow-400 rounded-full opacity-50"></div>
+    <div>    
+        <div class="absolute -top-10 -left-20 md:-top-40 md:-left-40 lg:-top-55 lg:-left-55 
+                    w-80 h-80 md:w-100 md:h-100 lg:w-160 lg:h-160 
+                    bg-gradient-to-br from-orange-300 via-orange-400 to-orange-300 
+                    rounded-full opacity-40 md:opacity-50 lg:opacity-60"></div>
+        <div class="absolute bottom-40 -left-8 md:top-50 md:-left-12 
+                    w-70 h-70 md:w-70 md:h-70 lg:w-110 lg:h-110 
+                    bg-gradient-to-br from-orange-300 via-orange-400 to-orange-300 
+                    rounded-full opacity-30 md:opacity-35 lg:opacity-40"></div>
+        
+        <div class="absolute top-20 -right-10 md:top-1/4 md:-right-15 lg:top-1/6 lg:-right-20 
+                    w-60 h-60 md:w-60 md:h-60 lg:w-80 lg:h-80 
+                    bg-gradient-to-br from-yellow-300 via-orange-200 to-yellow-400 
+                    rounded-full opacity-30 md:opacity-40 lg:opacity-50"></div>
+        
+        <div class="absolute -bottom-15 -right-10 md:-bottom-20 md:-right-8 lg:-bottom-30 lg:-right-10 
+                    w-90 h-90 md:w-80 md:h-80 lg:w-110 lg:h-110 
+                    bg-gradient-to-br from-yellow-300 via-cyan-900 to-yellow-400 
+                    rounded-full opacity-30 md:opacity-40 lg:opacity-50"></div>
     </div>
   )
 }
